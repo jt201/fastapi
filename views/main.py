09a9dd2c8ec8f71d01f1ba2caef109527998e6cd
jt201 <201@studio-jt.co.kr>
2022-12-06 16:49:46 +0900

    리스트 데이터 연동

--- views/main.py
+++ views/main.py
@@ -1,11 +1,41 @@
+from dataclasses import dataclass
+from datetime import datetime
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from typing import List
 
 from . import templates
 
 router = APIRouter()
 
 
+@dataclass
+class TodoItem:
+    id: int
+    content: str
+    author: str
+    status: bool
+    created: datetime
+
+
+data: List[TodoItem] = [
+    TodoItem(
+        id=1,
+        content="Todo List Item",
+        author="201",
+        status=False,
+        created=datetime.fromisoformat("2022-01-01 00:00:00"),
+    ),
+    TodoItem(
+        id=1,
+        content="OLD Todo List Item",
+        author="201",
+        status=True,
+        created=datetime.fromisoformat("2021-12-31 13:50:00"),
+    ),
+]
+
+
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
